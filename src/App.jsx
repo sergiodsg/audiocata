8215c15f4467219eb3c6713c21bbbe7f9d5930c6
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-03-06 14:08:46 -0400

    added animated background and fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import HALO from "vanta/src/vanta.halo";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    HALO({
+      el: "#vanta",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      size: 2.6,
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="background-fallback h-screen">
+      <div id="vanta" className="h-screen">
+        <h1 className="text-3xl">React App</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
