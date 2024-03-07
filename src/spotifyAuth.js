1dafa192bceb60709a8e4fa5f0238d08fbd8575f
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-03-07 12:06:30 -0400

    changed uri to render

--- src/spotifyAuth.js
+++ src/spotifyAuth.js
@@ -20,7 +20,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "https://audiocata.vercel.app/callback");
+  params.append("redirect_uri", "https://audiocata.onrender.com/callback");
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
