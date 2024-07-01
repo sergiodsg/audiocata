e1a67627b2dfe18a0cdd4810c181159b54d8d0a1
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-07-01 12:07:16 -0400

    💬 updated text in login page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="flex justify-center fixed inset-x-0 bottom-0 m-2">
       {/* <p className=" text-xs rounded-md bg-gray-300 bg-opacity-40 p-2">by sergiodsg</p> */}
-      <p className=" text-xs text-center rounded-md bg-gray-300 bg-opacity-40 p-2 w-72">❕ This app is still in development. Want to try it? email me at sergiodasilva.dev@gmail.com so I can add your spotify account to the development program😅</p>
+      <p className=" text-xs text-center rounded-md bg-gray-300 bg-opacity-40 p-2">by @sergiodsg</p>
     </div>
   )
 }
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -5,7 +5,7 @@ export default function Login({ clientId }) {
   return (
     <div className="flex flex-col justify-center p-5 rounded-md bg-gray-300 bg-opacity-40">
       <h1 className="text-4xl text-center text-white mb-2">audiocata</h1>
-      <p className="mb-2">A listening habits analyzer :)</p>
+      <p className="mb-2">a listening habits analyzer :)</p>
       <button className="btn" onClick={() => redirectToAuthCodeFlow(clientId)}>
         Log in with <img src={SpotifyLogo} alt="Spotify" className="w-20" style={{minWidth: "70px"}} />
       </button>
